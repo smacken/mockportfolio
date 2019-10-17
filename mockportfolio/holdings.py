814158e306a2452030ceb4771da75bdbce56a83e
Scott Mackenzie <scott.mac.web@gmail.com>
2019-10-17 20:22:05 +1300

    random sampling

--- mockportfolio/holdings.py
+++ mockportfolio/holdings.py
@@ -3,8 +3,29 @@ import pandas as pd
 
 
 class Holdings(object):
+    ''' trading holdings generator '''
     def __init__(self):
         pass
 
     def generate(self):
+        ''' generate holdings history frame '''
         return pd.DataFrame()
+
+    def listings(self):
+        ''' get a dataframe of all listed tickers '''
+        df = pd.read_csv('data/ASXListedCompanies.csv', skiprows=[0, 1])
+        df.rename(
+            columns={
+                'Company name': 'Name',
+                'ASX code': 'Tick',
+                'GICS industry group': 'Industry'
+            }, inplace=True)
+        return df
+
+    def random_ticks(self, listings, tick_count=10):
+        ''' get a list of random tickers from the listings '''
+        df = listings.sample(n=tick_count)
+        return df
+
+    def portfolio(self):
+        pass
--- tests/holdings_test.py
+++ tests/holdings_test.py
@@ -3,7 +3,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from mockportfolio import Holdings
 import pandas as pd
-# import mockportfolio
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, "mockportfolio"))
@@ -26,3 +25,16 @@ def test_generate_builds_dataframe():
     holdings = Holdings()
     df = holdings.generate()
     assert isinstance(df, pd.DataFrame) is True
+
+
+def test_listings_returns_dataframe():
+    holdings = Holdings()
+    asx = holdings.listings()
+    assert asx.empty is not True
+
+
+def test_random_ticks():
+    hold = Holdings()
+    asx = hold.listings()
+    ticks = hold.random_ticks(asx)
+    assert len(ticks.Tick.values) == 10
