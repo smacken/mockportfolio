0bccb1831229b31561ffaacd71c898eb47fd2960
Scott Mackenzie <scott.mac.web@gmail.com>
2019-11-17 12:02:50 +1300

    version

--- mockportfolio/__init__.py
+++ mockportfolio/__init__.py
@@ -3,9 +3,10 @@
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-
+import time
 from .holdings import Holdings
 from .prices import Prices
 from .transactions import Transactions
 
 __all__ = ['Holdings', 'Prices', 'Transactions']
+__version__ = '0.1.0.' + str(round(time.time()))
